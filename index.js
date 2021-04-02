c95c03951c506478c318a6aa8aaef10576c5559a
Warakubami <na3na7ko1@gmail.com>
2021-04-02 12:10:59 +0900

    17

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
+
 /**
- * 17の倍数である場合 true を返す
- * @param {number} num
+ * 17の倍数である場合 ture を返す
+ * @param {number} num 
  */
-function isMultipleOfSeventeen(num) {
+
+function isMultipleOfSeventeen(num){
   return num % 17 === 0;
 }
 
 module.exports = {
   isMultipleOfSeventeen
-};
+}
